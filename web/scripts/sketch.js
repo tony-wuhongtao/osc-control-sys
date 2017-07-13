6f87f359af12f958ca438c0e501434ba9ba92365
strngluv <strngluv@example.com>
2017-07-13 11:22:10 +0800

    add gallery class to hold and display floaters

--- web/scripts/sketch.js
+++ web/scripts/sketch.js
@@ -1,21 +1,13 @@
 var img;
 var bgimg;
-var lantern;
-var floaters = new Array(5);
-var locations = new Array(floaters.length);
-var selected = (floaters.length-1)/2;
-
-var launched = false;
-
 var touch;
-var triggered = false;
-
 var xoff = 0;
 var noiseInc = 0.05;
+var gallery = new Gallery(3);
 
 function preload() {
-  img = loadImage('assets/lantern.png');
-  bgimg = loadImage('assets/background.png');
+  //img = loadImage('assets/Floater.png');
+  //bgimg = loadImage('assets/background.png');
 }
 
 function setup() {
@@ -25,74 +17,14 @@ function setup() {
   colorMode(RGB, 255, 255, 255, 255);
   textSize(60);
   textAlign(CENTER);
-  //lantern = new Lantern(windowWidth/2, windowHeight*2/3);
-
-  // create an array of floaters
-  for (var i = 0, offset = 300; i < floaters.length; i++) {
-    //
-    var xoff = i-((floaters.length-1)/2);
-    locations[i] = windowWidth/2 + offset * xoff;
-    floaters[i] = new Lantern(locations[i], windowHeight*2/3, selected == i, i);
-  }
+  gallery.init();
 }
 
 function draw() {
   background(0);
   //image(bgimg, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+  gallery.render();
 
-
-
-  //lantern.update();
-  //lantern.display();
-  /*
-  if (lantern.position.y < 0 && lantern.triggered == false) {
-    d3.cue(1);
-    lantern.triggered = true;
-  }
-  */
-
-  for (var i = 0; i < floaters.length; i++) {
-    var floater = floaters[i];
-
-    if (launched && floater.selected) {
-      var floating = createVector(0, -0.05);
-      floater.applyForce(floating);
-    }
-    floater.update();
-
-    var destLocation = floater.location;
-    var diff = (locations[destLocation] - floater.position.x);
-
-    if (floater.direction * diff < 0) {
-      diff *= -1;
-    }
-
-    // smoothing
-    if (floater.position.x != locations[destLocation]) {
-      floater.position.x += diff * 0.1;
-    }
-
-    if (floater.position.y < 0) {
-      fill(255);
-      textSize(60 + (sin(xoff) - 0.5)*3);
-      text("Please Refresh!", windowWidth/2, windowHeight/3);
-      if (!triggered) {
-        console.log("BOW!");
-      }
-      triggered = true;
-
-    }
-
-    floater.checkEdge();
-
-    if (!launched) {
-      floater.render();
-    } else if (i == selected) {
-      floater.render();
-    }
-
-  }
-  xoff += noiseInc;
 }
 
 
@@ -105,55 +37,136 @@ function touchEnded() {
   var diffY = touch.y - mouseY;
   var diffX = touch.x - mouseX;
   if (diffY > 300) {
-    for (var i = 0; i < floaters.length; i++) {
-      floaters[i].launch();
-    }
+    gallery.launched = true;
   }
 
-  if (!launched && diffX > 200) {
-    swipe(-1);// swipe left
-    console.log("swipe left");
+  if (!gallery.launched && diffX > 200) {
+    gallery.swipe(-1);// swipe left
   }
 
-  if (!launched && diffX < -200) {
-    swipe(1);
-    console.log("swipe right");
+  if (!gallery.launched && diffX < -200) {
+    gallery.swipe(1); // swite right
   }
   return false;
 }
 
 
-function swipe(direction) {
-  selected = (selected - direction + floaters.length) % floaters.length;
-  console.log(selected);
-  for (var i = 0; i < floaters.length; i++) {
-    floaters[i].location = (floaters[i].location + direction + floaters.length) % floaters.length;
-    floaters[i].direction = direction;
-    if (i == selected) {
-      floaters[i].selected = true;
-    } else {
-      floaters[i].selected = false;
+
+
+function Gallery(n) {
+  // gallery status flags
+  this.launched = false;
+  this.triggered = false;
+
+  // floaters array
+  this.floaters = new Array(n);
+  // location slots to hold floaters
+  this.slots = new Array(this.floaters.length);
+  // current selected floater index
+  this.selected = (this.floaters.length-1)/2;
+
+  Gallery.prototype.init = function () {
+    // create an array of floaters
+    for (var index = 0, offset = 300; index < this.floaters.length; index++) {
+      //
+      var xoff = index-((this.floaters.length-1)/2);
+      this.slots[index] = windowWidth/2 + offset * xoff;
+      this.floaters[index] = new Floater(this.slots[index], windowHeight*2/3, this.selected == index, index);
     }
-  }
+  };
+
+  Gallery.prototype.swipe = function (direction) {
+    // update selected floater index
+    this.selected = (this.selected - direction + this.floaters.length) % this.floaters.length;
+    console.log(this.selected);
+
+    // update individual floater's destination slot index
+    for (var currentIndex = 0; currentIndex < this.floaters.length; currentIndex++) {
+      this.floaters[currentIndex].destSlotLocation = (this.floaters[currentIndex].destSlotLocation + direction + this.floaters.length) % this.floaters.length;
+      this.floaters[currentIndex].direction = direction;
+      if (currentIndex == this.selected) {
+        this.floaters[currentIndex].selected = true;
+      } else {
+        this.floaters[currentIndex].selected = false;
+      }
+    }
+  };
+
+  Gallery.prototype.render = function () {
+    for (var i = 0; i < this.floaters.length; i++) {
+      var floater = this.floaters[i];
+
+      // apply floating force to the selected floater
+      if (this.launched && floater.selected) {
+        var floating = createVector(0, -0.05);
+        floater.applyForce(floating);
+      }
+      floater.update();
+
+      // move if swipe
+      var dest = floater.destSlotLocation;
+      var diff = (this.slots[dest] - floater.position.x);
+
+      // if move towards different direction, reverse it
+      if (floater.direction * diff < 0) {
+        diff *= -1;
+      }
+
+      // smoothing movement
+      if (floater.position.x != this.slots[floater.destSlotLocation]) {
+        floater.position.x += diff * 0.1;
+      }
+
+      // floater move out of upper border
+      if (floater.position.y < 0) {
+        // pop up a hint to refresh
+        fill(255);
+        stroke(4);
+        // animation text
+        textSize(60 + (sin(xoff) - 0.5)*3);
+        xoff += noiseInc;
+        text("Please Refresh!", windowWidth/2, windowHeight/3);
+        if (!this.triggered) {
+          // fire a message
+          console.log("BOW!");
+        }
+        this.triggered = true;
+      }
+      floater.checkEdge();
+
+      // when not launched, display all floaters
+      if (!this.launched) {
+        floater.render();
+      } else if (i == this.selected) {
+        // if launched only display selected floater
+        floater.render();
+      }
+    }
+  };
+
+
 }
 
 
-var Lantern = function(x, y, s, l) {
+function Floater(xPos, yPos, isSelected, destSlotLocation) {
 
-  this.position = createVector(x, y);
+  // motion-wise properties
+  this.position = createVector(xPos, yPos);
   this.velocity = createVector(0, 0);
   this.acceleration = createVector(0, 0);
+
+  // display-wise properties
   this.c = color(random(255), random(255), random(255));
   this.alpha = 255;
   this.img = img;
-  this.selected = false;
-  this.triggered =  false;
-  this.destination = createVector(x, 0);
-  this.selected = s;
-  this.direction = 0;
-  this.location = l;
 
-  Lantern.prototype.render = function () {
+  // gallery-wise properties
+  this.selected = isSelected;
+  this.direction = 0;
+  this.destSlotLocation = destSlotLocation;
+
+
+  Floater.prototype.render = function () {
     // display image
     //image(this.img, this.position.x, this.position.y, this.img.width, this.img.height);
     if (this.selected) {
@@ -164,38 +177,26 @@ var Lantern = function(x, y, s, l) {
     }
 
     fill(this.c);
-    if(launched && !this.selected) {
-      //fill(this.c, 127);
-      this.alpha -= 1;
-      if (this.alpha < 0) {
-        this.alpha = 0;
-      }
-
-    }
     rect(this.position.x, this.position.y, 200, 200);
   };
 
-  Lantern.prototype.update = function() {
+  Floater.prototype.update = function() {
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.acceleration.mult(0);
   };
 
-  Lantern.prototype.applyForce = function (force) {
+  Floater.prototype.applyForce = function (force) {
     this.acceleration.add(force);
   };
 
-  Lantern.prototype.launch = function() {
-    launched = true;
-  };
-
-  Lantern.prototype.checkEdge = function () {
+  Floater.prototype.checkEdge = function () {
     if (this.position.x < -windowWidth*0.5) {
       this.position.x = windowWidth*1.5;
     }
     if (this.position.x > windowWidth*1.5) {
       this.position.x = -windowWidth*0.5;
     }
-  }
+  };
 
-};
+}
